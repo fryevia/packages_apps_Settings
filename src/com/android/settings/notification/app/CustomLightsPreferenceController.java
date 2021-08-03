Havoc-OS/android_packages_apps_Settings

Settings: Fix Custom light summary

src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.provider.Settings;
 import androidx.preference.Preference;
 
+import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.RestrictedSwitchPreference;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -36,6 +37,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
     private static final String KEY_CUSTOM_LIGHT = "custom_light";
 
     private int mLedColor = 0;
+    private ColorPickerPreference mCustomLight;
 
     public CustomLightsPreferenceController(Context context, NotificationBackend backend) {
         super(context, backend);
@@ -60,13 +62,13 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
 
     public void updateState(Preference preference) {
         if (mChannel != null) {
-             //light color pref
-            ColorPickerPreference mCustomLight = (ColorPickerPreference) preference;
+            mCustomLight = (ColorPickerPreference) preference;
             int defaultLightColor = mContext.getResources().getColor(com.android.internal.R.color.config_defaultNotificationColor);
             mCustomLight.setDefaultColor(defaultLightColor);
             mLedColor = (mChannel.getLightColor() != 0 ? mChannel.getLightColor() : defaultLightColor);
             mCustomLight.setAlphaSliderEnabled(false);
             mCustomLight.setNewPreviewColor(mLedColor);
+            updateSummary();
         }
     }
 
@@ -77,6 +79,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             mChannel.setLightColor(mLedColor);
             saveChannel();
             showLedPreview();
+            updateSummary();
         }
         return true;
     }
@@ -106,4 +109,12 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
     }
 
+    private void updateSummary() {
+        String lightColorHex = String.format("#%06x", (0xFFFFFF & mLedColor));
+        if (lightColorHex.equals("#ffffff")) {
+            mCustomLight.setSummary(R.string.default_string);
+        } else {
+            mCustomLight.setSummary(lightColorHex);
+        }
+    }
 }

fryevia/packages_apps_Settings

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/android/settings/notification/app/CustomLightsPreferenceController.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.provider.Settings;
 import androidx.preference.Preference;
 
+import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settings.notification.NotificationBackend;
 
@@ -38,6 +39,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
     private static final String KEY_CUSTOM_LIGHT = "custom_light";
 
     private int mLedColor = 0;
+    private ColorPickerPreference mCustomLight;
 
     public CustomLightsPreferenceController(Context context, NotificationBackend backend) {
         super(context, backend);
@@ -62,13 +64,13 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
 
     public void updateState(Preference preference) {
         if (mChannel != null) {
-             //light color pref
-            ColorPickerPreference mCustomLight = (ColorPickerPreference) preference;
+            mCustomLight = (ColorPickerPreference) preference;
             int defaultLightColor = mContext.getResources().getColor(com.android.internal.R.color.config_defaultNotificationColor);
             mCustomLight.setDefaultValue(defaultLightColor);
             mLedColor = (mChannel.getLightColor() != 0 ? mChannel.getLightColor() : defaultLightColor);
             mCustomLight.setAlphaSliderEnabled(false);
             mCustomLight.setNewPreviewColor(mLedColor);
+            updateSummary();
         }
     }
 
@@ -79,6 +81,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
             mChannel.setLightColor(mLedColor);
             saveChannel();
             showLedPreview();
+            updateSummary();
         }
         return true;
     }
@@ -108,4 +111,12 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
     }
 
+    private void updateSummary() {
+        String lightColorHex = String.format("#%06x", (0xFFFFFF & mLedColor));
+        if (lightColorHex.equals("#ffffff")) {
+            mCustomLight.setSummary(R.string.color_default);
+        } else {
+            mCustomLight.setSummary(lightColorHex);
+        }
+    }
 }